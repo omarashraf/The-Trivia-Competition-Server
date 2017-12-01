858248265df4087c8ce3ef59713e2425040340c2
Omar Ashraf <omar.mohamed1@emc.com>
2017-12-01 20:42:22 +0200

    no email/username checking;

--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -33,7 +33,7 @@ export class RegisterComponent implements OnInit {
     an error is prompted to the user.
   */
   onSubmit(): void {
-    if (/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/.test(this.email) && this.username !== "" && this.username !== undefined) {
+    if (this.username !== "" && this.username !== undefined) {
       let registrationData = 'username=' + this.username + '&email=' + this.email;
       this.registerNewUser(registrationData);
     }
@@ -44,7 +44,7 @@ export class RegisterComponent implements OnInit {
 
   // register new user and validate that there is no error
   registerNewUser(registrationData): void {
-    this.http.post("http://35.161.224.180:3000/register", registrationData, { headers: this.headers }).toPromise().then((res) => {
+    this.http.post("http://localhost:3000/register", registrationData, { headers: this.headers }).toPromise().then((res) => {
       console.log("RES --> ", res["_body"]);
       if (res["_body"] === "error") {
         this.errorRegistration = true;
