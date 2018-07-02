0a4624a521200009119751fe547e9d9069d08e88
hadeelfouad <11076441+hadeelfouad@users.noreply.github.com>
2018-07-02 17:36:18 +0200

    Feature 158522581 (#4)
    * code refactoring.
    
    * UI enhancements.

--- express.js
+++ express.js
@@ -5,35 +5,19 @@ var mongoDB = "mongodb://127.0.0.1:27017/riseup_db";
 var app = express();
 var cors = require('cors');
 var bodyParser = require("body-parser");
-const validate = require('express-validation');
+var validate = require('express-validation');
 
 var User = require("./models/user");
 var userCtrl = require("./controllers/user_ctrl");
 var userValidation = require("./validations/user_validations");
+var userRoutes = require("./routes/user_routes");
 
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(cors());
 
 // endpoint for testing
 app.get("/", userCtrl.testEndpoint);
-
-/*
-  - get the ranking of a certain number of players
-  - need to have the number of top players (in the body of the request) required to be returned
-*/
-app.post("/leaderboard", userCtrl.getLeaderboard);
-
-/*
-  - get a specific user
-  - need to have the email in the body of the request
-*/
-app.get('/user', userCtrl.getUser);
-
-/*
-  - update score of specific user
-  - need to have the score in the body of the request
-*/
-app.put("/score", userCtrl.updateScore);
 
 /*
   - add a new user in the db
@@ -41,6 +25,8 @@ app.put("/score", userCtrl.updateScore);
   - score is set to 0 initially
 */
 app.post("/register", validate(userValidation.register), userCtrl.registerUser);
+
+app.use('/users', userRoutes);
 
 // app is running on port 3000
 // TODO: set port among env variables
--- frontend/src/app/question/question.component.ts
+++ frontend/src/app/question/question.component.ts
@@ -61,11 +61,9 @@ export class QuestionComponent implements OnInit {
       */
       this.optionSelected = "";
       this.loginService.getCurrentUserInfo(this.currentUser).subscribe((res) => {
-        this.currentScore = res.json()[0]["score"];
+        this.currentScore = res.json()["score"];
         this.currentScore++;
-        scoreAndUsername = 'score=' + this.currentScore
-                         + '&username=' + this.currentUser;
-        this.loginService.updateScore(scoreAndUsername).subscribe((res) => {
+        this.loginService.updateScore(this.currentUser, this.currentScore).subscribe((res) => {
           this.questionManipulation.topPlayers("3").subscribe((res) => {
             this.top3Players = res.json();
           });
@@ -78,7 +76,7 @@ export class QuestionComponent implements OnInit {
         this.router.navigate(['./result']);
       }
       else {
-        localStorage.setItem('current', JSON.stringify( {username: this.currentUser, qIndex: this.currentQuestionIndex }));
+        localStorage.setItem('current', JSON.stringify( {email: this.currentUser, qIndex: this.currentQuestionIndex }));
       }
     }
     else {
@@ -89,7 +87,7 @@ export class QuestionComponent implements OnInit {
       else {
         // in case of wrong answer, the result view is prompted with current score
         this.loginService.getCurrentUserInfo(this.currentUser).subscribe((res) => {
-          this.currentScore = res.json()[0]["score"];
+          this.currentScore = res.json()["score"];
         });
         this.questionManipulation.wrongAnswer();
         this.router.navigate(['./result']);
@@ -158,7 +156,10 @@ export class QuestionComponent implements OnInit {
 
   // get current user from localStorage
   getCurrentUser(): void {
-    this.currentUser = JSON.parse(localStorage.getItem('current'))["username"];
+    this.currentUser = JSON.parse(localStorage.getItem('current'))["email"];
+    this.loginService.getCurrentUserInfo(this.currentUser).subscribe((res) => {
+      this.currentScore = res.json()["score"];
+    });
   }
 
   /*
