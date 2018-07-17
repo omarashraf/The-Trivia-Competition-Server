eade99a066a7e89f16c8714ffae4c9db3e9539f6
hadeelfouad <11076441+hadeelfouad@users.noreply.github.com>
2018-07-17 15:56:28 +0200

    Feature 158757965 (#7)
    * Admin login UI
    
    * admin can register
    
    * admin can login
    
    * admin can invite other admins.

--- config/config.js
+++ config/config.js
@@ -1,9 +1,12 @@
+//loads environment variables from a .env file into process.env
 require("dotenv").config();
 const ENV = process.env;
 
 const config = 
 {
-    jwtSecret: ENV.JWT_SECRET
+    jwtSecret: ENV.JWT_SECRET,
+    defaultPassword: ENV.DEFAULT_PASSWORD,
+    adminLoginUrl: ENV.ADMIN_LOGIN_URL
 };
 
 module.exports = config;
