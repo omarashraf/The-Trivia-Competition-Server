e39f0c3a99e460cc5b974fef92cf29823178e65b
hadeelfouad <11076441+hadeelfouad@users.noreply.github.com>
2018-07-31 13:50:03 +0200

    Feature 159298684 (#13)
    * using winston logger.
    
    * remove console.log
    
    * updated README.md

--- express.js
+++ express.js
@@ -1,23 +1,39 @@
 var express = require("express");
 var mongoose = require("mongoose");
-var mongoDB = "mongodb://127.0.0.1:27017/riseup_db";
-
 var app = express();
 var cors = require('cors');
 var bodyParser = require("body-parser");
 var validate = require('express-validation');
+var winston = require('winston');
+var expressWinston = require('express-winston');
+var config = require('./config/config')
+var winstonInstance = require('./config/winston.config')
 
-var User = require("./models/user");
 var userCtrl = require("./controllers/user_ctrl");
 var userValidation = require("./validations/user_validations");
 var userRoutes = require("./routes/user_routes");
 var questionRoutes = require("./routes/question_routes");
 var AdminRoutes = require("./routes/admin_routes")
 
-// app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
-app.use("/admin", AdminRoutes);
+
+// enable detailed API logging in dev env
+if (config.env === 'development') {
+  expressWinston.requestWhitelist.push('body'); 
+  expressWinston.responseWhitelist.push('body');
+  app.use(expressWinston.logger({
+    transports: [
+      new winston.transports.Console({
+        json: true,
+        colorize: true,
+        timestamp: true
+      })
+    ],
+    msg: 'HTTP {{req.method}} {{req.url}} {{res.statusCode}} {{res.responseTime}}ms'
+  })
+  );
+};
 
 // endpoint for testing
 app.get("/", userCtrl.testEndpoint);
@@ -29,13 +45,12 @@ app.get("/", userCtrl.testEndpoint);
 */
 app.post("/register", validate(userValidation.register), userCtrl.registerUser);
 app.use('/users', userRoutes);
-
+app.use("/admin", AdminRoutes);
 app.use('/questions', questionRoutes);
 
 // app is running on port 3000
-// TODO: set port among env variables
-app.listen(3000, function() {
-  console.log("Listening on port 3000..");
+app.listen(config.port, function () {
+  winstonInstance.info('listening on port', config.port);
 });
 
 app.use((err, req, res, next) => {
@@ -43,8 +58,8 @@ app.use((err, req, res, next) => {
 });
 
 // db connection through mongoose and printing status in console
-mongoose.connect(mongoDB).then(()=>{
-	console.log("Connected to Database");
-}).catch((err)=>{
-	console.log("Connection to Database error\n ", err);
+mongoose.connect(config.mongoDB).then(() => {
+  winstonInstance.info('Connected to database');
+}).catch((err) => {
+  winstonInstance.error("Connection to Database error\n ", err);
 });
