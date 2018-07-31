d3de3ec57776c37ccbf880753e7066904055406e
hadeelfouad <Hadeel.Fouad@emc.com>
2018-07-31 13:29:54 +0200

    using winston logger.

--- config/config.js
+++ config/config.js
@@ -6,7 +6,10 @@ const config =
 {
     jwtSecret: ENV.JWT_SECRET,
     defaultPassword: ENV.DEFAULT_PASSWORD,
-    adminLoginUrl: ENV.ADMIN_LOGIN_URL
+    adminLoginUrl: ENV.ADMIN_LOGIN_URL,
+    env: ENV.ENVIRONMENT,
+    port: ENV.PORT,
+    mongoDB: ENV.MONGODB
 };
 
 module.exports = config;
