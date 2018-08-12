58cd0418493041ac8de5a83e1bb7c7b485e50e1d
Mohamed <Omar.Mohamed1@emc.com>
2018-08-12 11:27:59 +0200

    removing the frontend folder to another repo

--- frontend/src/app/register/register.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
-
-@NgModule({
-    imports: [ RouterModule, CommonModule ]
-})
-
-export class RegisterModule {}
