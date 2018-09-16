10307f85b36cc1abb2680526c3dfb69e06aa3408
hadeelfouad <Hadeel.Fouad@emc.com>
2018-09-16 15:04:10 +0200

    player can register using dell domain only

--- validations/user_validations.js
+++ validations/user_validations.js
@@ -2,7 +2,7 @@ const Joi = require('joi');
 module.exports = {
     register: {
         body: {
-            email: [Joi.string().required().regex(/^[a-zA-Z0-9_.+-]+@(?:(?:[a-zA-Z0-9-]+\.)?[a-zA-Z]+\.)?(dell|emc)\.com$/)],
+            email: [Joi.string().required().regex(/^[a-zA-Z0-9_.+-]+@(?:(?:[a-zA-Z0-9-]+\.)?[a-zA-Z]+\.)?(dell)\.com$/)],
         }
     }
 }
